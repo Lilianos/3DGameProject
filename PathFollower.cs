b6edc7fda1e5fafbe6605704a8c6418bdac6d98e
Lilianos <31817228+Lilianos@users.noreply.github.com>
2018-03-31 01:15:48 +0200

    Add files via upload

---
+++ PathFollower.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFollower : MonoBehaviour {
+
+    public Transform[] path;
+    public float speed = 5.0f;
+    public float reachDist = 1.0f;
+    public int currentPoint = 0;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+        float dist = Vector3.Distance(path[currentPoint].position, transform.position);
+
+        transform.position = Vector3.Lerp(transform.position, path[currentPoint].position, Time.deltaTime * speed);
+
+        if (dist <= reachDist)
+        {
+            currentPoint++;
+        }
+
+        if (currentPoint >= path.Length)
+        {
+            currentPoint = 0;
+        }
+		
+	}
+
+    void OnDrawGizmos()
+    {
+        if (path.Length < 0)
+        for (int i = 0; i < path.Length; i++)
+            {
+                if(path[i] != null)
+                {
+                    Gizmos.DrawSphere(path[i].position, reachDist);
+                }
+            }
+    }
+}
